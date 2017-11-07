3ab3169e50c133918dcd370302be66f79efee2a3
Michelle B <mjblevins7@gmail.com>
2017-11-07 03:13:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name)
+  return kittens
+}

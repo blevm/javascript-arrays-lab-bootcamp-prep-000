8106a30aa1f8e8e04489ade638a2a71252b0715e
Michelle B <mjblevins7@gmail.com>
2017-11-07 03:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,11 +31,9 @@ function appendKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(-1)
-  return kittens
+  return kittens.slice(-1)
 }
 
 function removeFirstKitten() {
-  kittens.slice(0, kittens.length - 1)
-  return kittens
+  return kittens.slice(0, kittens.length - 1)
 }
